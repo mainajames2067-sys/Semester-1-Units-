33ebb386a17f4a53e9f3288b9fe8db602013af09
mainajames2067-sys <mainajames2067@gmail.com>
2025-09-26 10:34:57 +0300

    Add files via upload

---
+++ Units .c
@@ -0,0 +1,21 @@
+//simple c program (hello world);
+/*
+Name    :James maina
+Reg no:CT101/G/26470/25
+Date :18/9/2025
+description:hellow world program
+*/
+//main function
+#include<stdio.h>
+
+int main() {
+    printf("fundermentals of ICT\n");
+    printf("mathematics for science\n");
+    printf("Discrete mathematics\n");
+    printf("Analogue electronics\n");
+    printf("Physics\n");
+    printf("computer architecture\n");
+    printf("introduction to programing and problem solving\n");
+
+    return 0;
+}
