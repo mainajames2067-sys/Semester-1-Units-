e8c7669f1582ab3672b8513b10f10b0dd96aa629
mainajames2067-sys <mainajames2067@gmail.com>
2025-10-30 12:21:08 +0300

    Add files via upload

---
+++ 1D_Array.c
@@ -0,0 +1,18 @@
+/*
+Name:James maina
+Reg no:CT101/G/26470/25
+Date:28/10/2025
+Description:one D array 
+*/
+#include<stdio.h>
+
+int main(){
+//declaration and initialization 
+    int z;
+    int scores[10]={10,20,30,40,50,60,70,80,90};
+    //for loop
+    for(z=0;z<10;z++){
+    printf("%d\t",scores[z]);
+    }
+    return 0;
+}
